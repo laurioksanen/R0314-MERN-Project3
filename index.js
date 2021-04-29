c6271b50c15b9ab5929ebe7e074d232dde199e46
Lauri Oksanen <lauri.oksanen1@hotmail.com>
2021-04-29 19:23:28 +0300

    cors vittuun

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var mongoose = require("mongoose");
 var cors = require("cors");
 
 var app = express();
-app.use(cors());
+//app.use(cors());
 
 var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
